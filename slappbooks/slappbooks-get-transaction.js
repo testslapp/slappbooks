ef9f91c0f615c236a7344785e4873a5ea6d37365
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-17 12:34:43 +0530

    Test commit

--- slappbooks/slappbooks-get-transaction.js
+++ slappbooks/slappbooks-get-transaction.js
@@ -4,14 +4,15 @@ exports.handler = function(event, context, callback) {
 	console.log(transactionId);
     let trs = [ { date: '01/16/2018', checkNo: '', notes: '', amount: 100, isCredit: false, entityName: 'Cash' , trId: '12345678' },
 						 { date: '01/16/2018', checkNo: '', notes: '', amount: '(100)', isCredit: true, entityName: 'Bank', trId: '87654321' } ];
-	callback(null, (trs) =>  {
+	console.log("Data retrieved", trs);
+    callback(null, (trs) =>  {
 
 		let response = {
         "statusCode": 200,
         "headers": {
             "my_header": "my_value"
         },
-        "body": JSON.stringify(data),
+        "body": JSON.stringify(trs),
         "isBase64Encoded": false
         };
         return response;
