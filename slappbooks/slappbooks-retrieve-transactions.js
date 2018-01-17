bf84c42351964684aa4471b11ebe263df9237877
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-17 10:32:13 +0530

    configured return object

--- slappbooks/slappbooks-retrieve-transactions.js
+++ slappbooks/slappbooks-retrieve-transactions.js
@@ -1,7 +1,9 @@
 let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
 	console.log(event);
-	let transactions = [ { date: '01/16/2018', checkNo: '', notes: '', amount: 100, isCredit: false, entityName: 'Cash' },
-						 { date: '01/16/2018', checkNo: '', notes: '', amount: '(100)', isCredit: true, entityName: 'Bank' } ];
+	let transactions = { rows : [ { date: '01/16/2018', checkNo: '', notes: '', amount: 100, isCredit: false, entityName: 'Cash' },
+						 { date: '01/16/2018', checkNo: '', notes: '', amount: '(100)', isCredit: true, entityName: 'Bank' } ],
+						 pages : 1
+						};
 	callback(null, transactions);
 }
