3db58a44a0b4791742bc213f71b0072e7bfd6709
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-17 12:12:55 +0530

    changed response

--- slappbooks/slappbooks-get-transaction.js
+++ slappbooks/slappbooks-get-transaction.js
@@ -2,18 +2,18 @@ let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
 	let transactionId = event.queryStringParameters.id;
 	console.log(transactionId);
-	callback(null, function (err, data) {
-		if (err) {
-			  callback(err, null);
-		} else {
-			 let response = {
+    let trs = [ { date: '01/16/2018', checkNo: '', notes: '', amount: 100, isCredit: false, entityName: 'Cash' , trId: '12345678' },
+						 { date: '01/16/2018', checkNo: '', notes: '', amount: '(100)', isCredit: true, entityName: 'Bank', trId: '87654321' } ];
+	callback(null, (trs) =>  {
+
+		let response = {
         "statusCode": 200,
         "headers": {
             "my_header": "my_value"
         },
         "body": JSON.stringify(data),
         "isBase64Encoded": false
-    };
-           callback(null, response);
-		}});
+        };
+        return response;
+		});
 }
