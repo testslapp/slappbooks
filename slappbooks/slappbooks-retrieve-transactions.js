c08bced1dae8cd143ee85fe480f228695fa45894
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-17 11:55:38 +0530

    added trId

--- slappbooks/slappbooks-retrieve-transactions.js
+++ slappbooks/slappbooks-retrieve-transactions.js
@@ -1,8 +1,8 @@
 let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
 	console.log(event);
-	let transactions = { rows : [ { date: '01/16/2018', checkNo: '', notes: '', amount: 100, isCredit: false, entityName: 'Cash' },
-						 { date: '01/16/2018', checkNo: '', notes: '', amount: '(100)', isCredit: true, entityName: 'Bank' } ],
+	let transactions = { rows : [ { date: '01/16/2018', checkNo: '', notes: '', amount: 100, isCredit: false, entityName: 'Cash' , trId: '12345678' },
+						 { date: '01/16/2018', checkNo: '', notes: '', amount: '(100)', isCredit: true, entityName: 'Bank', trId: '87654321' } ],
 						 pages : 1
 						};
 	callback(null, transactions);
