b15eb96b5f579801e7f5f9e579ace9ec889794b6
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-17 09:45:51 +0530

    changed import

--- slappbooks/slappbooks-lambda.js
+++ slappbooks/slappbooks-lambda.js
@@ -1,6 +1,6 @@
 let AWS = require('aws-sdk');
 let connectionManager = require('./ConnectionManager');
-let SL = require('slappforge-aws');
+let SL = require('@slappforge/slappforge-sdk');
 const rds = new SL.AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
 	console.log(event);
