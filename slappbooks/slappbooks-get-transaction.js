dd61350d23fd18dc903cf47bcb56ab4afdb996e6
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-17 12:39:59 +0530

    Fixing the response issue

--- slappbooks/slappbooks-get-transaction.js
+++ slappbooks/slappbooks-get-transaction.js
@@ -5,16 +5,12 @@ exports.handler = function(event, context, callback) {
     let trs = [ { date: '01/16/2018', checkNo: '', notes: '', amount: 100, isCredit: false, entityName: 'Cash' , trId: '12345678' },
 						 { date: '01/16/2018', checkNo: '', notes: '', amount: '(100)', isCredit: true, entityName: 'Bank', trId: '87654321' } ];
 	console.log("Data retrieved", trs);
-    callback(null, (trs) =>  {
-
-		let response = {
+    callback(null, {
         "statusCode": 200,
         "headers": {
             "my_header": "my_value"
         },
         "body": JSON.stringify(trs),
         "isBase64Encoded": false
-        };
-        return response;
-		});
+    });
 }
