3db67cf19f251167a3bc3b1caf8545928a9b3c20
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-17 21:21:16 +0530

    set_id added

--- slappbooks/slappbooks-lambda.js
+++ slappbooks/slappbooks-lambda.js
@@ -41,7 +41,7 @@ exports.handler = function (event, context, callback) {
 		// You can pass the existing connection to this function.
 		// A new connection will be creted if it's not present as the third param 
 
-		let sql = 'INSERT INTO transaction (transaction_id, date, entity_id, is_credit, cheque_no, voucher_no, amount, notes, reconcile) VALUES (?,?,?,?, ?, ?, ?, ?, ?);'
+		let sql = 'INSERT INTO transaction (transaction_id, set_id, date, entity_id, is_credit, cheque_no, voucher_no, amount, notes, reconcile) VALUES (?,?,?,?,?, ?, ?, ?, ?, ?);'
 		transactions.forEach( (transaction, index) => {
 
 			rds.query({
@@ -63,7 +63,7 @@ exports.handler = function (event, context, callback) {
 					rds.query({
 						identifier: 'slappbooksdb',
 						query: sql,
-						inserts: [transaction.trId, transaction.date, entity_id, transaction.isCredit, transaction.checkNo, transaction.voucherNo, transaction.amount, transaction.notes, transaction.reconcile]
+						inserts: [transaction.trId, transaction.setId, transaction.date, entity_id, transaction.isCredit, transaction.checkNo, transaction.voucherNo, transaction.amount, transaction.notes, transaction.reconcile]
 					}, function (error, results, connection) {
 						if (error) {
 							connection.rollback();
