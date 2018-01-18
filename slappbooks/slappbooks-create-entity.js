c9f529edd960ae49c28e9387b93d1eee975f316c
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-18 10:37:39 +0530

    changed event.slice to event

--- slappbooks/slappbooks-create-entity.js
+++ slappbooks/slappbooks-create-entity.js
@@ -3,7 +3,7 @@ let connectionManager = require('./ConnectionManager');
 let SL = require('@slappforge/slappforge-sdk');
 const rds = new SL.AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
-	let entity = event.slice();
+	let entity = event;
 
 	let sql = 'INSERT INTO entity (name, currency, type) values (?, ?, ?)';
 	// Replace the query with the actual query
