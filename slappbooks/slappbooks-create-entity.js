60df76aba5ee3bcbbe97126aecb73168a36af8d3
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-18 10:31:51 +0530

    created the entity insert query

--- slappbooks/slappbooks-create-entity.js
+++ slappbooks/slappbooks-create-entity.js
@@ -1,5 +1,27 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
-	console.log(event);
-	callback(null,'Successfully executed');
+let connectionManager = require('./ConnectionManager');
+let SL = require('@slappforge/slappforge-sdk');
+const rds = new SL.AWS.RDS(connectionManager);
+exports.handler = function (event, context, callback) {
+	let entity = event.slice();
+
+	let sql = 'INSERT INTO entity (name, currency, type) values (?, ?, ?)';
+	// Replace the query with the actual query
+	// You can pass the existing connection to this function.
+	// A new connection will be creted if it's not present as the third param 
+	rds.query({
+		identifier: 'slappbooksdb',
+		query: sql,
+		inserts: [entity.entity, entity.currency, entity.entityType]
+	}, function (error, results, connection) {
+		if (error) {
+			console.log("Error occurred while inserting the entity", error);
+			throw error;
+		} else {
+			console.log("Successfully inserted the entity");
+			console.log(results);
+		}
+		connection.end();
+		callback(null, 'Successfully executed');
+	});
 }
