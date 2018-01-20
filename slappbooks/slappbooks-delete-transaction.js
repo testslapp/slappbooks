0af7690046c33a10f532777ac3dd661960a4afe2
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-20 11:47:47 +0530

    delete transaction

---
+++ slappbooks/slappbooks-delete-transaction.js
@@ -0,0 +1,40 @@
+let AWS = require('aws-sdk');
+let connectionManager = require('./ConnectionManager');
+let SL = require('@slappforge/slappforge-sdk');
+const rds = new SL.AWS.RDS(connectionManager);
+exports.handler = function (event, context, callback) {
+	let setId = event.setId;
+
+	rds.beginTransaction({
+		instanceIdentifier: 'slappbooksdb'
+	}, function (error, connection) {
+		if (error) { throw err; }
+
+
+		let sql = 'DELETE FROM transaction WHERE setId=?';
+		// Replace the query with the actual query
+		// You can pass the existing connection to this function.
+		// A new connection will be creted if it's not present as the third param 
+		rds.query({
+			identifier: 'slappbooksdb',
+			query: 'sql',
+			inserts: [setId]
+		}, function (error, results, connection) {
+			if (error) {
+				connection.rollback();
+				console.log("Error occurred while deleting the transaction");
+				throw error;
+			} else {
+				console.log("Successfully deleted the transaction");
+				connection.commit();
+				console.log(results);
+			}
+
+			connection.end();
+		});
+
+	});
+
+
+	callback(null, 'Successfully executed');
+}
