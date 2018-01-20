8b06527d3934fb716faa0b7926cd64f807dbd866
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-20 16:14:37 +0530

    fixed issue

--- slappbooks/slappbooks-get-monthly-transactions.js
+++ slappbooks/slappbooks-get-monthly-transactions.js
@@ -22,6 +22,33 @@ exports.handler = function (event, context, callback) {
 		case "February":
 			month = '02';
 			break;
+		case "March":
+			month = '03';
+			break;
+		case "April":
+			month = '01';
+			break;
+		case "May":
+			month = '05';
+			break;
+		case "June":
+			month = '06';
+			break;
+		case "July":
+			month = '07';
+			break;
+		case "August":
+			month = '08';
+			break;
+		case "September":
+			month = '09';
+			break;
+		case "October":
+			month = '10';
+			break;
+		case "November":
+			month = '11';
+			break;
 		case "December":
 			month = '12';
 			break;
