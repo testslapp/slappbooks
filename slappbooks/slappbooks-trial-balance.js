182ab2ddec0f71fb1571c43edbb9568c31a9e650
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-25 09:42:54 +0530

    changed to absolute value

--- slappbooks/slappbooks-trial-balance.js
+++ slappbooks/slappbooks-trial-balance.js
@@ -6,7 +6,7 @@ const rds = new SL.AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
 
     let entries = [];
-	let sql = "SELECT E.name AS name, ABS(SUM( IF(T.is_credit='1', -1*T.amount, T.amount))) AS value FROM transaction T INNER JOIN entity E on T.entity_id=E.id GROUP BY E.id;";
+	let sql = "SELECT E.name AS name, SUM( IF(T.is_credit='1', -1*T.amount, T.amount)) AS value FROM transaction T INNER JOIN entity E on T.entity_id=E.id GROUP BY E.id;";
 	rds.query({
 		instanceIdentifier: 'slappbooksdb',
 		query: sql,
@@ -22,7 +22,7 @@ exports.handler = function (event, context, callback) {
 				let entry = {
 					name: result.name,
 					isCredit: result.value < 0 ? true: false,
-					value: result.value
+					value: Math.abs(result.value)
 				}
 				entries.push(entry);
 			});
