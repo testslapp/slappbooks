6543520dd44a2bb49b7053b1b2ab546dcf626b5c
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-25 16:17:32 +0530

    removed switch case

--- slappbooks/slappbooks-get-monthly-transactions.js
+++ slappbooks/slappbooks-get-monthly-transactions.js
@@ -17,47 +17,6 @@ exports.handler = function (event, context, callback) {
 	let year = postObject.year;
 	let month = postObject.month;
 
-	switch(month) {
-		case "January":
-			month = '01';
-			break;
-		case "February":
-			month = '02';
-			break;
-		case "March":
-			month = '03';
-			break;
-		case "April":
-			month = '04';
-			break;
-		case "May":
-			month = '05';
-			break;
-		case "June":
-			month = '06';
-			break;
-		case "July":
-			month = '07';
-			break;
-		case "August":
-			month = '08';
-			break;
-		case "September":
-			month = '09';
-			break;
-		case "October":
-			month = '10';
-			break;
-		case "November":
-			month = '11';
-			break;
-		case "December":
-			month = '12';
-			break;
-		default:
-			month = '01';
-	}
-
 	// retrieve transactions between the selected time frame
 	let sql = 'SELECT * FROM transaction T INNER JOIN entity E ON T.entity_id = E.id WHERE E.name =? AND date BETWEEN ? AND ?  LIMIT ?,?';
 
