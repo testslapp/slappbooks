2f383e2e722c9d332884d966e1d1c57fac49107b
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-29 14:41:11 +0530

    API renamed to slappbooksapi2

--- slappbooks/slappbooks-insert-transaction-with-currency.js
+++ slappbooks/slappbooks-insert-transaction-with-currency.js
@@ -2,6 +2,7 @@ let AWS = require('aws-sdk');
 let connectionManager = require('./ConnectionManager');
 let SL = require('@slappforge/slappforge-sdk');
 const rds = new SL.AWS.RDS(connectionManager);
+
 exports.handler = function (event, context, callback) {
 
 	postObject = event;
