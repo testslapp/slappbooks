fc62815d2c003489a4a3a761cdfbc4ccb1db0657
testslapp <35335548+testslapp@users.noreply.github.com>
2018-01-29 12:21:18 +0530

    created a new database connection

--- slappbooks/ConnectionManager.js
+++ slappbooks/ConnectionManager.js
@@ -3,14 +3,7 @@ module.exports=function(){
     this.dbConnections = [];
     
     this.dbConnections["slappbooksdb"] = {
-        host: "slappbooksdb.cvmlduwb8e2g.us-east-1.rds.amazonaws.com",
-        port: "3306",
-        user: "slappbooksuser",
-        password: "12345678",
-        database: "slappbooksdb",
-    };
-    this.dbConnections["slappbooksdb"] = {
-        host: "slappbooksdb.cvmlduwb8e2g.us-east-1.rds.amazonaws.com",
+        host: process.env.EndPoint_slappbooksdb,
         port: "3306",
         user: "slappbooksuser",
         password: "12345678",
